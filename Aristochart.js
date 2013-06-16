473d47d9a0fe1d5d89f870030b90ec421a376f7d
Adrian Cooney <cooney.adrian@gmail.com>
2013-06-16 21:11:17 +0100

    Moved this.defaults to Aristochart.theme.defaults for better management

--- Aristochart.js
+++ Aristochart.js
@@ -21,167 +21,8 @@ var Aristochart = function(element, options, theme) {
 	// Edit some options
 	if(options.width && !options.height) options.height = Math.floor(options.width * 0.67);
 
-	// The default options.
-	this.defaults = {
-		width: 640,
-		height: 400,
-		margin: 70,
-		padding: 20,	
-
-		fill: {
-			index: 0,
-			render: Aristochart.line.fill,
-			fillToBaseLine: true,
-		},
-
-		axis: {
-			index: 1,
-			render: Aristochart.axis.line,
-
-			x: {
-				steps: 5,
-				render: Aristochart.axis.line,
-			},
-
-			y: {
-				steps: 10,
-				render: Aristochart.axis.line,
-			}
-		},
-
-		tick: {
-			index: 2,
-			render: Aristochart.tick.line
-		},
-
-		line: {
-			index: 3,
-			render: Aristochart.line.line
-		},
-
-		point: {
-			index: 4,
-			render: Aristochart.point.circle
-		},
-
-		label: {
-			index: 5,
-			render: Aristochart.label.text,
-			x: {
-				step: 1
-			},
-			y: {
-				step: 1
-			}
-		},
-
-		title: {
-			index: 6,
-			render: Aristochart.title.text,
-			x: "x",
-			y: "y"
-		},
-
-		style: {
-			default: {
-				point: {
-					stroke: "#000",
-					fill: "#fff",
-					radius: 4,
-					width: 3,
-					visible: true
-				},
-
-				line: {
-					stroke: "#298281",
-					width: 3,
-					fill: "rgba(150, 215, 226, 0.4)",
-					visible: true
-				},
-
-				axis: {
-					stroke: "#ddd",
-					width: 3,
-					visible: true,
-
-					x: {
-						visible: true,
-						fixed: true
-					},
-
-					y: {
-						visible: true,
-						fixed: true
-					}
-				},
-
-				tick: {
-					align: "middle", //"outside", "inside",
-					stroke: "#ddd",
-					width: 2,
-					minor: 10,
-					major: 15,
-					visible: true,
-
-					x: {
-						fixed: true
-					},
-
-					y: {
-						fixed: true	
-					}
-				},
-
-				label: {
-					x: {
-						font: "Helvetica",
-						fontSize: 14,
-						fontStyle: "normal",
-						color: "#000",
-						align: "center",
-						baseline: "bottom",
-						offsetY: 8,
-						offsetX: 3,
-						visible: true,
-						fixed: true
-					},
-
-					y: {
-						font: "Helvetica",
-						fontSize: 10,
-						fontStyle: "normal",
-						color: "#000",
-						align: "center",
-						baseline: "bottom",
-						offsetY: 8,
-						offsetX: 8,
-						visible: true,
-						fixed: true
-					}
-				},
-
-				title: {
-					color: "#777",
-					font: "georgia",
-					fontSize: "16",
-					fontStyle: "italic",
-					visible: true,
-
-					x: {
-						offsetX: 0,
-						offsetY: 120,
-						visible: true
-					},
-
-					y: {
-						offsetX: -135,
-						offsetY: 10,
-						visible: true
-					}
-				}
-			}
-		}
-	};
+	//Set the defaults
+	this.defaults = Aristochart.themes.default;
 
 	this.options = options;
 	this.canvas = element;
@@ -652,3 +493,168 @@ Aristochart.title = {
  * @type {Object}
  */
 Aristochart.themes = {};
+
+/**
+ * Default theme.
+ * @type {Object}
+ */
+Aristochart.themes.default = {
+	width: 640,
+	height: 400,
+	margin: 70,
+	padding: 20,	
+
+	fill: {
+		index: 0,
+		render: Aristochart.line.fill,
+		fillToBaseLine: true,
+	},
+
+	axis: {
+		index: 1,
+		render: Aristochart.axis.line,
+
+		x: {
+			steps: 5,
+			render: Aristochart.axis.line,
+		},
+
+		y: {
+			steps: 10,
+			render: Aristochart.axis.line,
+		}
+	},
+
+	tick: {
+		index: 2,
+		render: Aristochart.tick.line
+	},
+
+	line: {
+		index: 3,
+		render: Aristochart.line.line
+	},
+
+	point: {
+		index: 4,
+		render: Aristochart.point.circle
+	},
+
+	label: {
+		index: 5,
+		render: Aristochart.label.text,
+		x: {
+			step: 1
+		},
+		y: {
+			step: 1
+		}
+	},
+
+	title: {
+		index: 6,
+		render: Aristochart.title.text,
+		x: "x",
+		y: "y"
+	},
+
+	style: {
+		default: {
+			point: {
+				stroke: "#000",
+				fill: "#fff",
+				radius: 4,
+				width: 3,
+				visible: true
+			},
+
+			line: {
+				stroke: "#298281",
+				width: 3,
+				fill: "rgba(150, 215, 226, 0.4)",
+				visible: true
+			},
+
+			axis: {
+				stroke: "#ddd",
+				width: 3,
+				visible: true,
+
+				x: {
+					visible: true,
+					fixed: true
+				},
+
+				y: {
+					visible: true,
+					fixed: true
+				}
+			},
+
+			tick: {
+				align: "middle", //"outside", "inside",
+				stroke: "#ddd",
+				width: 2,
+				minor: 10,
+				major: 15,
+				visible: true,
+
+				x: {
+					fixed: true
+				},
+
+				y: {
+					fixed: true	
+				}
+			},
+
+			label: {
+				x: {
+					font: "Helvetica",
+					fontSize: 14,
+					fontStyle: "normal",
+					color: "#000",
+					align: "center",
+					baseline: "bottom",
+					offsetY: 8,
+					offsetX: 3,
+					visible: true,
+					fixed: true
+				},
+
+				y: {
+					font: "Helvetica",
+					fontSize: 10,
+					fontStyle: "normal",
+					color: "#000",
+					align: "center",
+					baseline: "bottom",
+					offsetY: 8,
+					offsetX: 8,
+					visible: true,
+					fixed: true
+				}
+			},
+
+			title: {
+				color: "#777",
+				font: "georgia",
+				fontSize: "16",
+				fontStyle: "italic",
+				visible: true,
+
+				x: {
+					offsetX: 0,
+					offsetY: 120,
+					visible: true
+				},
+
+				y: {
+					offsetX: -135,
+					offsetY: 10,
+					visible: true
+				}
+			}
+		}
+	}
+};
