f2080f6b66f2b313087ce23f6586b2b8a6df2798
Adrian Cooney <cooney.adrian@gmail.com>
2013-07-19 14:55:55 +0100

    Damn innerText. Fixes #3

--- doc/script/Examples.js
+++ doc/script/Examples.js
@@ -7,6 +7,6 @@ window.addEventListener("DOMContentLoaded", function() {
 	var code = document.getElementById("example-code");
 	if(!code) return alert("Please place the code for the example on the page and give the script tag the class 'example-code'.");
 	
-	document.getElementById("input-code").innerHTML = Sintax.syntaxHighlight(code.innerText.substr(1, code.innerText.length - 1)); //Chop off that first newline	
+	document.getElementById("input-code").innerHTML = Sintax.syntaxHighlight(code.textContent.substr(1, code.textContent.length - 1)); //Chop off that first newline	
 
 });
